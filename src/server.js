3456fdb9c6e3fd152a5d76ca4add6ddc1da0504e
aaloe7438 <109014340+aaloe7438@users.noreply.github.com>
2022-11-27 19:31:38 +0900

    ~4.1까지 강의
    ~4.1까지 강의

--- src/server.js
+++ src/server.js
@@ -1,45 +1,34 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-///////////////////
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if(url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed. You may continue.");
-  next();
-}
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-const handleLogin = (req, res) => {
-  return res.send("Login Here");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to priave rounge");
-}
-
-
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
 
-////////////////
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
